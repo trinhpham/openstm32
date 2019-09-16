d2c0af0022c4d444454b95dbffd4b2ac095d1cd8
Trinh Pham <trinh.pham@logigear.com>
2019-09-16 12:00:46 +0700

    Update path

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,10 @@ RUN wget http://www.ac6-tools.com/downloads/SW4STM32/install_sw4stm32_linux_64bi
     mkdir -p /usr/local/OpenStm32/ && \
     ./install_sw4stm32_linux_64bits-latest.run -f auto-install.xml
 
+RUN PLUGIN_PATH=`find /usr/local/OpenStm32/plugins -maxdepth 1 -type d -name 'fr.ac6.mcu.externaltools.arm-none.linux64*' -print -quit` && \
+    MCU_PATH=`find ${PLUGIN_PATH}/tools -maxdepth 1 -type f -name 'st-gnu-arm-gcc*' -print -quit` && \
+    tar -jxvf $MCU_PATH -C ${PLUGIN_PATH}/tools
+
 WORKDIR /workspace
 
 CMD [ "/build.sh"]
--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo ============================
+echo Starting up...
+export MCU_FOLDER=`find /usr/local/OpenStm32/plugins -maxdepth 3 -type d -name 'st-gnu-arm-gcc*' -print -quit`
+export PATH=$MCU_FOLDER/bin:$PATH
+echo Current PATH: $PATH
+echo
+
+echo ============================
 echo Import project from src..
 /usr/local/OpenStm32/eclipse -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /workspace -import /src
-
+echo
+echo ============================
 echo Start build...
 /usr/local/OpenStm32/eclipse -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data /workspace -cleanBuild all
